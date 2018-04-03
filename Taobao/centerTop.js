02f5d9d2485252ccc71143b9f1097ab42284a24d
陈勇 <528264030@qq.com>
2018-04-03 17:47:34 +0800

    修改

--- Taobao/centerTop.js
+++ Taobao/centerTop.js
@@ -1,6 +1,13 @@
 window.onload = function(){
 	var oMainCenter = document.getElementsByClassName('main-center')[0];
 	var oImg = oMainCenter.getElementsByTagName('img');
+	var oTop = document.getElementsByClassName('top')[0];
+	var oLeftFirst = oTop.getElementsByClassName('left-first')[0];
+	var oLeftUl = oTop.getElementsByClassName('left-ul')[0];
+	var oRightFirst = oTop.getElementsByClassName('right-first')[0];
+	var oRightSecond = oTop.getElementsByClassName('right-second')[0];
+	var oRightOne = oTop.getElementsByClassName('right-one')[0];
+	var oRightTwo = oTop.getElementsByClassName('right-two')[0];
 	var arrImg2 = ['img/bottom1.jpg','img/bottom2.jpg','img/bottom3.jpg','img/bottom4.jpg']
 	var arrImg1 = ['img/top1.jpg','img/top2.jpg','img/top3.jpg','img/top4.jpg']
 	var ospan = oMainCenter.getElementsByTagName('span')[0];
@@ -8,7 +15,41 @@ window.onload = function(){
 	var num = 0;
 	var len = oLi.length;
 	var arrlen2 = arrImg2.length;
-	var timer;
+	var timer,timerTop;
+	//top部分功能简单实现
+		//left
+	oLeftFirst.onmouseover = function(){
+		oLeftUl.style.display = 'block';
+	}
+	oLeftFirst.onmouseout = function(){
+		 timerTop = setTimeout(function(){oLeftUl.style.display = 'none';
+			
+		},1000);
+	}
+	oLeftUl.onmouseover = function(){
+		clearTimeout(timerTop);
+		// alert(1);
+		oLeftUl.style.display = 'block';
+		
+	}
+	oLeftUl.onmouseout = function(){
+		oLeftUl.style.display = 'none';
+	}
+		//right
+	oRightFirst.onmouseover = function(){
+		oRightOne.style.display = 'block';
+	}
+	oRightFirst.onmouseout = function(){
+		oRightOne.style.display = 'none';
+	}
+	oRightSecond.onmouseover = function(){
+		oRightTwo.style.display = 'block';
+	}
+	oRightSecond.onmouseout = function(){
+		oRightTwo.style.display = 'none';
+	}
+
+	//main部分功能
 	ospan.innerHTML = num + '/' + arrlen2;
 	// alert(oLi[num]);
 	oLi[num].style.backgroundColor = '#f40';
