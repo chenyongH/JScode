aaf8f65c765b7baa069d60c3a59b7be832ca54be
陈勇 <528264030@qq.com>
2018-04-15 23:16:53 +0800

    知问项目本地服务器上传数据

--- jquery知问项目/js/first.js
+++ jquery知问项目/js/first.js
@@ -6,18 +6,29 @@ $(function(){
 	});
 	$('#header input').button();
 
+	// loading 数据提交时的弹窗效果
+	$('#loading').dialog({
+		autoOpen:false,
+		modal:true,
+		closeOnEscape:false,
+		resizable:false,
+		draggable:false,
+		width:180,
+		height:50,
+	}).parent().find('.ui-widget-header').hide();
+
 	// 表单注册
 		$('#reg').dialog({
 			title:"知问注册",
 			buttons:{
-				"确定":function(){
-					alert("提交中");
+				"提交":function(){
+					$(this).submit();
 				},
 				"取消":function(){
 					$(this).dialog("close");
 				}
 			},
-			autoOpen:true,
+			autoOpen:false,
 			show:'puff',
 			hide:'puff',
 			height:400,
@@ -25,14 +36,106 @@ $(function(){
 			modal:true,
 
 			closeText:"取消注册"
-		});	
-	
+		}).buttonset().validate({
+
+			submitHandler:function(form){
+				$(form).ajaxSubmit({
+					url:'add.php',
+					type:'POST',
+					beforeSubmit:function(formDate,jqForm,options){
+						$('#loading').dialog('open');
+						$('#reg').dialog('widget').find('button').eq(2).button('disable');
+					},
+					success:function(responseText,statusText){
+						if(responseText){
+							$('#reg').dialog('widget').find('button').eq(2).button('enable');
+							$('#loading').html('数据上传成功!');
+							setTimeout(function(){
+								$('#reg').dialog('close');
+								$('#loading').dialog('close');
+								$('#reg').resetForm();
+								$('#reg span').removeClass('reg-span');
+								$('#loading').html('数据上传中...');
+							},500);
+						}
+					},
+				});
+			},
+
+
+
+			highlight:function(element,errorClass){
+				$(element).css('border','1px solid #f40');
+				$(element).parent().find('span').removeClass('reg-span');
+			},
+			unhighlight:function(element,errorClass){
+				$(element).css('border','1px solid #666');
+				$(element).parent().find('span').addClass('reg-span');
+			},
+
+			showErrors:function(errorMap,erroList){
+				var errors = this.numberOfInvalids();
+				if(errors > 0){
+					$('#reg').dialog('option','height',errors*20 +400);
+				}else{
+					$('#reg').dialog('option','height',400);
+				}
+				this.defaultShowErrors();
+			},
+
+			errorLabelContainer:'ol.reg-error',
+			wrapper:'li',
+
+			rules:{
+				user:{
+					required:true,
+					minlength:2
+				},
+				password:{
+					required:true,
+					minlength:6,
+				},
+				password2:{
+					required:true,
+					equalTo:"#password"
+				},
+				email:{
+					// com:true,
+					required:true,
+					email:true
+				},
+
+			},
+			messages:{
+				user:{
+					required:"姓名不能为空",
+					minlength: $.validator.format('姓名不能少于{0}位'),
+				},
+				password:{
+					required:"密码不能为空",
+					minlength: $.validator.format('密码不能少于{0}位'),
+				},
+				password2:{
+					required:"确认密码不能为空",
+					equalTo: "你的密码确认输入不相同",
+					
+				},
+				email:{
+					required:"邮箱不能为空",
+					minlength: "请输入有效的电子邮件地址",
+				},
+			},
+		});
+		// $.validator.addMethod('com',function(){
+		// 	var email = ".com$";
+		// 	return this.optional(element)||(email.test(value));
+		// },'请输入正确的email地址');
+
+
 	$('#reg-a').click(function(){
 		$('#reg').dialog('open');
 	});
     
-	$('#reg').buttonset();
-	// $('#reg .reg-sex input').button();
 	// 日历
 	$('#date').datepicker({
 		showOn:'both',
@@ -52,13 +155,13 @@ $(function(){
 		}
 	});
 	// 表单注册自动补全
-	var  mailSource = ['aa@qq.com','bb@360.com','aa@360.com','bb@qq.com'];
-	$('#mail').autocomplete({
-		source: mailSource,
-		delay:0,
-		minlength:1,
+	// var  mailSource = ['aa@qq.com','bb@360.com','aa@360.com','bb@qq.com'];
+	// $('#emil').autocomplete({
+	// 	source: mailSource,
+	// 	delay:0,
+	// 	minlength:1,
 
-	});
+	// });
 
 
 
