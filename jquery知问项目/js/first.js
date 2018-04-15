496f0db35e456ab61e4f1dc3342264e837ae4abc
陈勇 <528264030@qq.com>
2018-04-15 11:46:58 +0800

    日历完善

--- jquery知问项目/js/first.js
+++ jquery知问项目/js/first.js
@@ -5,7 +5,8 @@ $(function(){
 		}
 	});
 	$('#header input').button();
-	
+
+	// 表单注册
 		$('#reg').dialog({
 			title:"知问注册",
 			buttons:{
@@ -29,18 +30,36 @@ $(function(){
 	$('#reg-a').click(function(){
 		$('#reg').dialog('open');
 	});
-
-
-
+    
 	$('#reg').buttonset();
 	// $('#reg .reg-sex input').button();
-	$('#date').datepicker();
+	// 日历
+	$('#date').datepicker({
+		showOn:'both',
+		showButtonPanel:true,
+		maxDate:0,
+		yearRange:'1940:2018',
+		changeYear:true,
+		changeMonth:true
+	});
+	$('.ui-datepicker-trigger').html('选择');
+	$('.ui-datepicker-trigger').button();
+
 	$('#reg input[title]').tooltip({
 		position:{
 			my:'left+10 center',
 			at:'right center'
 		}
 	});
+	// 表单注册自动补全
+	var  mailSource = ['aa@qq.com','bb@360.com','aa@360.com','bb@qq.com'];
+	$('#mail').autocomplete({
+		source: mailSource,
+		delay:0,
+		minlength:1,
+
+	});
+
 
 
 })
